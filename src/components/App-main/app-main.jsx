9db9b0e4a828ba3f4e1bce3ed109b54fabcc0e14
draketyt <lupen902@mail.ru>
2025-03-14 19:18:23 +0700

    nike pro

---
+++ src/components/App-main/app-main.jsx
@@ -0,0 +1,26 @@
+import {Tab} from "@ya.praktikum/react-developer-burger-ui-components";
+import React from "react";
+
+export const AppMain=  () =>{
+	const [current, setCurrent] = React.useState('one');
+
+	return(
+		<>
+			<main style={{}}>
+			<div style={{ display: 'flex' }}>
+				<Tab value='one' active={current === 'one'} onClick={setCurrent}>
+					Булки
+				</Tab>
+				<Tab value='two' active={current === 'two'} onClick={setCurrent}>
+					Соусы
+				</Tab>
+				<Tab value='three' active={current === 'three'} onClick={setCurrent}>
+					Начинки
+				</Tab>
+			</div>
+		</main>
+
+		</>
+	);
+
+}
--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,1 +1,55 @@
-export const AppHeader = () => <p>Test</p>;
+import {
+	BurgerIcon,
+	ListIcon,
+	Logo,
+	ProfileIcon,
+	Tab,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import React from 'react';
+export const AppHeader = () => {
+	// @ts-ignore
+	return (
+		<>
+			<header
+				style={{
+					background: '#1C1C21',
+				}}
+				className='p-5'>
+				<nav style={{}} className='pt-4 pb-4'>
+					<ul
+						style={{
+							textAlign:'center',
+							listStyle: 'none',
+							display: 'flex',
+							justifyContent: 'space-between',
+							flexDirection: 'row',
+							gap:'8px',
+							alignItems: 'center',
+							textWrap: 'nowrap',
+							fontWeight: '700',
+						}}>
+						<li className='nav__list-item primary pb-4 pt-4 pr-2 pl-5 '>
+							<BurgerIcon type='primary' className='icon' />
+							<span className='icon-li1'>Конструктор</span>
+						</li>
+
+						<li className='nav__list-item secondary pb-4 pt-4 pr-2 pl-5  '>
+							<ListIcon type='secondary' className='icon' />
+							<span>Лента заказов</span>
+						</li>
+						<li>
+						<Logo className='logo mr-25'/>
+							</li>
+
+
+						<li className='nav__list-item secondary pb-4 pt-4 pr-2 pl-5 mr-20'>
+							<ProfileIcon type='secondary' className='icon' />
+							<span>Личный кабинет</span>
+						</li>
+					</ul>
+				</nav>
+			</header>
+
+		</>
+	);
+};
