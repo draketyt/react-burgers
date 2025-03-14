801ca15741fee08724ec114835bc83699e012c20
draketyt <lupen902@mail.ru>
2025-03-14 20:40:12 +0700

    nike pro

--- src/components/App-main/app-main.jsx
+++ src/components/App-main/app-main.jsx
@@ -6,7 +6,8 @@ export const AppMain=  () =>{
 
 	return(
 		<>
-			<main style={{}}>
+			<main style={{textAlign:"left", display:'flex',alignContent:'center',justifyContent:'center', flexDirection:'column',alignItems:'center' }}>
+				<h1> составь бутербродиксы</h1>
 			<div style={{ display: 'flex' }}>
 				<Tab value='one' active={current === 'one'} onClick={setCurrent}>
 					Булки
