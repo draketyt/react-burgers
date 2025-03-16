219933f0de88e31f0a8dc58aca28e6a534f9126f
draketyt <lupen902@mail.ru>
2025-03-16 17:06:27 +0700

    сделал штуки

--- src/components/App-main/app-main.jsx
+++ src/components/App-main/app-main.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import { ConstructorElement, Tab, Button, CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+
 export const AppMain = () => {
 	const [current, setCurrent] = React.useState("one");
+	const [ingredients,setIngredients] = useState([
+		{ id: 2, text: "Соус традиционный галактический", price: 300 },
+		{ id: 3, text: "Мясо бессмертных моллюсков Protostomia", price: 300 },
+		{ id: 4, text: "Плоды Фалленианского дерева", price: 80 },
+		{ id: 5, text: "Хрустящие минеральные кольца", price: 80 },
+		{ id: 6, text: "Хрустящие минераль354ные кольца", price: 80 },
+		{ id: 7, text: "Хрустящие минеральные кольца", price: 80 },
+		{ id: 8, text: "Хрустящие минеральные кольца", price: 80 },
+
+
+	]);
+
 
 	return (
 		<main className='main' >
@@ -24,6 +38,7 @@ export const AppMain = () => {
 					<p className={`text_type_main-large Title`}>Булки</p>
 					<div className={'grid pt-6'}>
 						<ConstructorElement text="Краторная булка N-200i" price={20} />
+
 						<ConstructorElement text="Флуоресцентная булка R2-D3" price={20} />
 					</div>
 
@@ -36,18 +51,24 @@ export const AppMain = () => {
 			</div>
 
 			<div className={'right__Panel mt-25 ml-10'}>
-				<div className={'order__List'}>
+				<div>
+					<ul className={'order__List'}>
 					<ConstructorElement type="top" isLocked={true} text="Краторная булка N-200i (верх)" price={20} />
-					<ConstructorElement text="Соус традиционный галактический" price={300} />
-					<ConstructorElement text="Мясо бессмертных моллюсков Protostomia" price={300} />
-					<ConstructorElement text="Плоды Фалленианского дерева" price={80} />
-					<ConstructorElement text="Хрустящие минеральные кольца" price={80} />
+						{ingredients.map((ingredient) => (
+							<ConstructorElement
+								key={ingredient.id}
+								type={ingredient.type}
+								text={ingredient.text}
+								price={ingredient.price}
+							/>
+						))}
 					<ConstructorElement type="bottom" isLocked={true} text="Краторная булка N-200i (низ)" price={20} />
+					</ul>
 				</div>
 
-				<div className={'order__Summary mt-10 mr-7'}>
+				<div className={'order__Summary mt-5 mr-7 mb-10'}>
 					<div className={'price__Container mr-10'}>
-						<span className="text text_type_digits-medium">610</span>
+						<span className="text text_type_digits-medium pr-2">610</span>
 						<CurrencyIcon type="primary" />
 					</div>
 					<Button type="primary" size="medium" >
