7e18cf16d9ef1aed58ce17c656dc43f737eb461c
draketyt <lupen902@mail.ru>
2025-03-16 22:44:42 +0700

    сделал штуки

---
+++ src/components/app-main/burger-ingredients/Ingredients-group/Ingredients-group.jsx
@@ -0,0 +1,35 @@
+import {ConstructorElement, Tab} from "@ya.praktikum/react-developer-burger-ui-components";
+import React, {useState} from "react";
+const [current, setCurrent] = useState("one");
+
+export const IngredientsGroup= () =>{
+	return(<section className="left__panel">
+		<p className="text text_type_main-large mb-5 mt-10 title">Соберите бургер</p>
+		<tabs className={`tabs mb-4`} >
+			<Tab value="one" active={current === "one"} onClick={setCurrent}>
+				Булки
+			</Tab>
+			<Tab value="two" active={current === "two"} onClick={setCurrent}>
+				Соусы
+			</Tab>
+			<Tab value="three" active={current === "three"} onClick={setCurrent}>
+				Начинки
+			</Tab>
+		</tabs>
+
+		<div className="ingredients pt-10 custom-scroll" >
+			<p className="text_type_main-large title">Булки</p>
+			<div className="grid pt-6">
+				<ConstructorElement text="Краторная булка N-200i" price={20} />
+
+				<ConstructorElement text="Краторная булка N-200i" price={20} />
+			</div>
+
+			<p className="text_type_main-large title pt-10">Соусы</p>
+			<div className="grid pt-6">
+				<ConstructorElement text="Соус Spicy-X" price={30} />
+			</div>
+		</div>
+	</section>)
+}
+export default IngridientsGroup;
