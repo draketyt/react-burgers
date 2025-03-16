faec02878a8033bde20be93f5fd4ec9da36322d0
draketyt <lupen902@mail.ru>
2025-03-17 01:18:41 +0700

    сделал штуки

--- src/components/app-main/app-main.jsx
+++ src/components/app-main/app-main.jsx
@@ -1,11 +1,8 @@
-import React, {useEffect, useState} from "react";
-import { ConstructorElement,Button, CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import PropTypes from "prop-types";
 // @ts-ignore
 import {IngredientsGroup} from "./burger-ingredients/Ingredients-group/Ingredients-group";
 // @ts-ignore
 import {BurgerComposition} from "./burger-ingredients/burger-composition/burger-composition";
-
 export const AppMain = () => {
 
 	AppMain.propTypes = {
