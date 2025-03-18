a6cba653d79a77ecb4b5c138d1f7fbb94bd28bee
draketyt <lupen902@mail.ru>
2025-03-18 22:39:59 +0700

    update

--- src/components/app-main/burger-ingredients/Ingredients-group/Ingredients-group.jsx
+++ src/components/app-main/burger-ingredients/Ingredients-group/Ingredients-group.jsx
@@ -1,75 +1,7 @@
 import { CurrencyIcon, Tab} from "@ya.praktikum/react-developer-burger-ui-components";
 import React, {useState} from "react";
-
-function IngredientItemBun({ image, price, name }) {
-	const ingredients = {
-		success: true,
-		data: [
-			{
-				_id: "643d69a5c3f7b9001cfa093c",
-				name: "Краторная булка N-200i",
-				type: "bun",
-				price: 988,
-				image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
-			},
-			{
-				_id: "643d69a5c3f7b9001cfa093d",
-				name: "Флюоресцентная булка R2-D3",
-				type: "bun",
-				proteins: 44,
-				fat: 26,
-				carbohydrates: 85,
-				calories: 643,
-				price: 988,
-				image: "https://code.s3.yandex.net/react/code/bun-01.png",
-				image_mobile: "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
-				image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
-				__v: 0,
-			},
-			{
-				_id: "643d69a5c3f7b9001cfa0941",
-				name: "Биокотлета из марсианской Магнолии",
-				type: "main",
-				proteins: 420,
-				fat: 142,
-				carbohydrates: 242,
-				calories: 4242,
-				price: 424,
-				image: "https://code.s3.yandex.net/react/code/meat-01.png",
-				image_mobile: "https://code.s3.yandex.net/react/code/meat-01-mobile.png",
-				image_large: "https://code.s3.yandex.net/react/code/meat-01-large.png",
-				__v: 0,
-			},
-			{
-				_id: "643d69a5c3f7b9001cfa093e",
-				name: "Филе Люминесцентного тетраодонтимформа",
-				type: "main",
-				proteins: 44,
-				fat: 26,
-				carbohydrates: 85,
-				calories: 643,
-				price: 988,
-				image: "https://code.s3.yandex.net/react/code/meat-03.png",
-				image_mobile: "https://code.s3.yandex.net/react/code/meat-03-mobile.png",
-				image_large: "https://code.s3.yandex.net/react/code/meat-03-large.png",
-				__v: 0,
-			},
-			{
-				_id: "643d69a5c3f7b9001cfa0942",
-				name: "Соус Spicy-X",
-				type: "sauce",
-				proteins: 30,
-				fat: 20,
-				carbohydrates: 40,
-				calories: 30,
-				price: 90,
-				image: "https://code.s3.yandex.net/react/code/sauce-02.png",
-				image_mobile: "https://code.s3.yandex.net/react/code/sauce-02-mobile.png",
-				image_large: "https://code.s3.yandex.net/react/code/sauce-02-large.png",
-				__v: 0,
-			},
-		],
-	};
+import {ingredients} from '/src/utils/ingredients'
+function IngredientItem({ image, price, name }) {
 	return (
 
 		<div className="ingredient-item">
@@ -88,67 +20,10 @@ function IngredientItemBun({ image, price, name }) {
 
 
 }
-
-
 export const IngredientsGroup = () => {
 	const [current, setCurrent] = useState("one");
 
-	const ingredients = [
-		{
-			_id: "643d69a5c3f7b9001cfa093c",
-			name: "Краторная булка N-200i",
-			type: "bun",
-			price: 988,
-			image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
-		},
-		{
-			_id: "643d69a5c3f7b9001cfa093d",
-			name: "Флюоресцентная булка R2-D3",
-			type: "bun",
-			price: 988,
-			image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
-		},
-		{
-			_id: "643d69a5c3f7b9001cfa0942",
-			name: "Соус Spicy-X",
-			type: "sauce",
-			price: 90,
-			image_large: "https://code.s3.yandex.net/react/code/sauce-02-large.png",
-		},
-		{
-			_id: "643d69a5c3f7b9001cfa0941",
-			name: "Биокотлета из марсианской Магнолии",
-			type: "main",
-			price: 424,
-			image_large: "https://code.s3.yandex.net/react/code/meat-01-large.png",
-		},{
-			"_id": "643d69a5c3f7b9001cfa0947",
-			"name": "Плоды Фалленианского дерева",
-			"type": "main",
-			"proteins": 20,
-			"fat": 5,
-			"carbohydrates": 55,
-			"calories": 77,
-			"price": 874,
-			"image": "https://code.s3.yandex.net/react/code/sp_1.png",
-			"image_mobile": "https://code.s3.yandex.net/react/code/sp_1-mobile.png",
-			"image_large": "https://code.s3.yandex.net/react/code/sp_1-large.png",
-			"__v": 0
-		},  {
-			"_id": "643d69a5c3f7b9001cfa0945",
-			"name": "Соус с шипами Антарианского плоскоходца",
-			"type": "sauce",
-			"proteins": 101,
-			"fat": 99,
-			"carbohydrates": 100,
-			"calories": 100,
-			"price": 88,
-			"image": "https://code.s3.yandex.net/react/code/sauce-01.png",
-			"image_mobile": "https://code.s3.yandex.net/react/code/sauce-01-mobile.png",
-			"image_large": "https://code.s3.yandex.net/react/code/sauce-01-large.png",
-			"__v": 0
-		},
-	];
+
 
 	const buns = ingredients.filter((item) => item.type === "bun");
 	const sauces = ingredients.filter((item) => item.type === "sauce");
@@ -173,9 +48,9 @@ export const IngredientsGroup = () => {
 			<div className="ingredients pt-10 custom-scroll">
 				<p className="text_type_main-large title">Булки</p>
 				<div className="grid pt-6">
-					<ul className="ingredient-items">
+					<ul className="ingredient-items custom-scroll">
 						{buns.map((item) => (
-							<IngredientItemBun key={item._id} type="bun" image={item.image_large} price={item.price} name={item.name} />
+							<IngredientItem key={item._id} type="bun" image={item.image_large} price={item.price} name={item.name} />
 						))}
 					</ul>
 				</div>
@@ -184,9 +59,9 @@ export const IngredientsGroup = () => {
 			<div className="ingredients pt-10 custom-scroll">
 				<p className="text_type_main-large title">Соусы</p>
 				<div className="grid pt-6">
-					<ul className="ingredient-items">
+					<ul className="ingredient-items custom-scroll" >
 						{sauces.map((item) => (
-							<IngredientItemBun key={item._id} type="sauce" image={item.image_large} price={item.price} name={item.name} />
+							<IngredientItem key={item._id} type="sauce" image={item.image_large} price={item.price} name={item.name} />
 						))}
 					</ul>
 				</div>
@@ -195,9 +70,9 @@ export const IngredientsGroup = () => {
 			<div className="ingredients pt-10 custom-scroll">
 				<p className="text_type_main-large title">Начинки</p>
 				<div className="grid pt-6">
-					<ul className="ingredient-items">
+					<ul className="ingredient-items custom-scroll">
 						{mains.map((item) => (
-							<IngredientItemBun key={item._id} type="main" image={item.image_large} price={item.price} name={item.name} />
+							<IngredientItem key={item._id} type="main" image={item.image_large} price={item.price} name={item.name} />
 						))}
 					</ul>
 				</div>
