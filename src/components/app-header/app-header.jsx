772885a87839a3565e79e4d7d2193f3ee4338756
draketyt <lupen902@mail.ru>
2025-03-20 04:44:50 +0700

    update

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -6,12 +6,10 @@ import {
 } from '@ya.praktikum/react-developer-burger-ui-components';
 import React from 'react';
 export const AppHeader = () => {
-	// @ts-ignore
 	return (
 		<>
 			<header
-				style={{
-				}}
+
 				className='header p-5'>
 				<nav  className='pl-25 pt-4 pb-4 pr-25' >
 					<ul
@@ -26,20 +24,25 @@ export const AppHeader = () => {
 						}}>
 						<li className='nav__list-item primary pb-4 pt-4 pr-2 pl-5 '>
 							<BurgerIcon type='primary' className='icon' />
-							<span className='icon-li1'><p className="text text_type_main-medium">
-Конструктор</p></span>
+							<span className='icon-li1'>
+								<p className="text text_type_main-medium">Конструктор</p>
+							</span>
 						</li>
 
 						<li className='nav__list-item secondary pb-4 pt-4 pr-2 pl-5  '>
 							<ListIcon type='secondary' className='icon' />
-							<span><p className="text text_type_main-medium">Лента заказов</p></span>
+							<span>
+								<p className="text text_type_main-medium">Лента заказов</p>
+							</span>
 						</li>
 						<Logo className='logo mr-25' style={{flexGrow:'1'}}/>
 
 
-						<li className='nav__list-item secondary pb-4 pt-4 pr-2 pl-5 mr-20'>
+						<li className='nav__list-item secondary pb-4 pt-4 pr-2 pl-5 '>
 							<ProfileIcon type='secondary' className='icon' />
-							<span><p className="text text_type_main-medium">Личный кабинет</p></span>
+							<span>
+								<p className="text text_type_main-medium">Личный кабинет</p>
+							</span>
 						</li>
 					</ul>
 				</nav>
