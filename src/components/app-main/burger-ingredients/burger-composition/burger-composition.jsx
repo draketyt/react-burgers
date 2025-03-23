f525dd6c641772db6a5673e9c9f21064e9e4648f
draketyt <lupen902@mail.ru>
2025-03-23 21:40:05 +0700

    refactored

--- src/components/app-main/burger-ingredients/burger-composition/burger-composition.jsx
+++ src/components/app-main/burger-ingredients/burger-composition/burger-composition.jsx
@@ -11,7 +11,12 @@ export const BurgerComposition=()=>{
 			.then((data) => {
 				setIngredients(data.data);
 			})
-			.catch((err) => console.error("Ошибка загрузки:", err));
+			.then(res => {
+				if (res.ok) {
+					return res.json();
+				}
+				return Promise.reject(`Ошибка ${res.status}`);
+			})
 	}, []);
 	return(
 
